995b4c67a92fcec4ea38b5ed7ffe3cfb9e502b01
astrolab <astrolabscig@gmail.com>
2026-06-27 10:08:36 +0000

    fix: image url type import

--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -1,5 +1,5 @@
 import createImageUrlBuilder from '@sanity/image-url'
-import { SanityImageSource } from '@sanity/image-url/lib/types/types'
+import type { SanityImageSource } from 'sanity'
 import { dataset, projectId } from '../env'
 
 const builder = createImageUrlBuilder({ projectId, dataset })
