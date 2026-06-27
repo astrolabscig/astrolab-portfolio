67e2d247db5e396b6496023c88dbaf5dd8ec47db
astrolab <astrolabscig@gmail.com>
2026-06-27 10:19:28 +0000

    fix: remove strict image source type

--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -1,9 +1,8 @@
 import createImageUrlBuilder from '@sanity/image-url'
-import type { SanityImageSource } from 'sanity'
 import { dataset, projectId } from '../env'
 
 const builder = createImageUrlBuilder({ projectId, dataset })
 
-export function urlFor(source: SanityImageSource) {
+export function urlFor(source: any) {
   return builder.image(source)
 }
